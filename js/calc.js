9c040a896105ddcbcbe05bdccc88adb7128ed09d
krm111 <ruben.cuba@ucsp.edu.pe>
2018-04-19 00:51:22 -0500

    prototype

---
+++ js/calc.js
@@ -0,0 +1,42 @@
+var cadena = "";
+ 
+function calcular(){		
+ 
+	var entrada = document.getElementById("txt_entrada");			
+	var cad = (cadena === "") ?  entrada.value : "(" + cadena  + ")" + entrada.value;							
+
+	try{
+		var resultado = eval(cad);					
+		document.getElementById("p_resultado").innerHTML = resultado;
+		entrada.value = "";
+		cadena = cad;
+		document.getElementById("p_cadena").innerHTML = cadena;
+	}catch(err){					
+		alert("Syntax Error");				
+	}
+ 
+}		
+ 
+function borrar(){
+	var entrada = document.getElementById("txt_entrada");			
+	entrada.value = "";		
+	cadena = "";			
+	document.getElementById("p_resultado").innerHTML = "0";
+	document.getElementById("p_cadena").innerHTML = cadena;
+}	
+ 
+function identificar_tecla(evento){
+	// Verificar si se presiona la tecla "Enter"
+	if(evento.keyCode==13){
+		calcular();		
+		// Verificar si se presiona la tecla "Esc"				
+	}else if(evento.keyCode==27){
+		borrar();
+	}
+}
+ 
+function tecla (texto){
+	var entrada = document.getElementById("txt_entrada");
+	entrada.value+=texto;
+}
+
